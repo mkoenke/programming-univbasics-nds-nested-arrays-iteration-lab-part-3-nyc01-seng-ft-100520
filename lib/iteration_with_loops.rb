37cc18ea1cbca38c9fa5f9fda842a0953d1c9589
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-16 15:05:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   while outer_index < src.count do
     inner_index = 0
     while inner_index < src[outer_index].count do
-      if src[outer_index][inner_index].class == String 
+      if src[outer_index][inner_index].class == String
         only_strings << src[outer_index][inner_index]
       end
       inner_index += 1
